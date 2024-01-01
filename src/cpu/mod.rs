0df2a148afdbf1051fb8e168739cfcb9fe1f2706
Andrew X. Shah <drewshah0@gmail.com>
2024-01-01 11:20:17 -0700

    feat(cpu/execute): +add_hl

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -2,18 +2,19 @@ pub mod flags_register;
 pub mod instruction;
 pub mod registers;
 
-use instruction::{ArithmeticTarget, Instruction};
-use registers::Registers;
+use self::instruction::{ADDHLTarget, ArithmeticTarget, Instruction};
+use self::registers::Registers;
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 struct CPU {
     pub registers: Registers,
+    sp: u16,
 }
 
 impl CPU {
     pub fn execute(&mut self, instruction: Instruction) {
         match instruction {
-            Instruction::ADD(target) => match target {
+            Instruction::ADD(register) => match register {
                 ArithmeticTarget::C => {
                     let value = self.registers.c;
                     let new_value = self.add(value);
@@ -21,18 +22,40 @@ impl CPU {
                 }
                 _ => {}
             },
+            Instruction::ADDHL(register) => {
+                let value = match register {
+                    ADDHLTarget::BC => self.registers.get_bc(),
+                    ADDHLTarget::DE => self.registers.get_de(),
+                    ADDHLTarget::HL => self.registers.get_hl(),
+                    ADDHLTarget::SP => self.sp,
+                };
+                let res = self.add_hl(value);
+                self.registers.set_hl(res);
+            }
             _ => {}
         }
     }
 
     pub fn add(&mut self, value: u8) -> u8 {
-        let (new_value, overflow) = self.registers.a.overflowing_add(value);
+        let (res, carry) = self.registers.a.overflowing_add(value);
 
-        self.registers.f.zero = new_value == 0;
+        self.registers.f.zero = res == 0;
         self.registers.f.subtract = false;
-        self.registers.f.carry = overflow;
+        self.registers.f.carry = carry;
         self.registers.f.half_carry = (self.registers.a & 0xF) + (value & 0xF) > 0xF;
 
-        new_value
+        res
+    }
+
+    pub fn add_hl(&mut self, value: u16) -> u16 {
+        let hl = self.registers.get_hl();
+        let (res, carry) = hl.overflowing_add(value);
+
+        self.registers.f.subtract = false;
+        self.registers.f.carry = carry;
+        let mask = 0b111_1111_1111;
+        self.registers.f.half_carry = (value & mask) + (hl & mask) > mask;
+
+        res
     }
 }
